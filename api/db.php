25bb9070eaee94a62a9574c1048a388ed6062a83
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-18 23:08:24 +0700

    chore(cleanup): remove .github CI folder per owner request

--- api/db.php
+++ api/db.php
@@ -18,7 +18,29 @@ function getPDO()
 
     // If SSL CA file is provided, set MYSQL_ATTR_SSL_CA
     if (!empty($cfg['ssl_ca'])) {
-        $options[PDO::MYSQL_ATTR_SSL_CA] = $cfg['ssl_ca'];
+        $ssl = $cfg['ssl_ca'];
+        $ssl_path = null;
+
+        // If the value is an existing file path, use it.
+        if (file_exists($ssl)) {
+            $ssl_path = $ssl;
+        } elseif (strpos($ssl, '-----BEGIN CERTIFICATE-----') !== false) {
+            // If the secret contains the PEM contents, write it to a temp file and use that path.
+            $hash = substr(sha1($ssl), 0, 12);
+            $tmp = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'db_ca_' . $hash . '.pem';
+            if (!file_exists($tmp)) {
+                file_put_contents($tmp, $ssl);
+                @chmod($tmp, 0644);
+            }
+            $ssl_path = $tmp;
+        } else {
+            // Fallback: treat as a path (may fail later if invalid)
+            $ssl_path = $ssl;
+        }
+
+        if ($ssl_path) {
+            $options[PDO::MYSQL_ATTR_SSL_CA] = $ssl_path;
+        }
     }
 
     return new PDO($dsn, $user, $pass, $options);
