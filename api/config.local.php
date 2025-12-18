6471875b2b72cedc9173b38ffc136cffb7dbb1fd
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-18 23:51:46 +0700

    chore: commit local edits and include config.local.php for deploy

---
+++ api/config.local.php
@@ -0,0 +1,10 @@
+<?php
+// Local WAMP development database configuration
+return [
+    'host' => 'sql212.infinityfree.com',
+    'port' => 3306,
+    'dbname' => 'if0_40013336_student_db',
+    'user' => 'if0_40013336',
+    'pass' => 'QlVyrvJdXLeMsW',
+    'ssl_ca' => null,
+];
